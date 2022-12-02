6fec6fb766fb571ff3563bf401b171478a96b815
pkruzikova <p.kruziku@gmail.com>
2022-12-02 20:28:47 +0000

    added 3 important files

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from setuptools import setup, find_packages
 setup(name="Sagittal brain analysis", 
     version="0.1.0", 
     packages=find_packages(exclude=['*tests']),
+    install_requires = ['numpy'],
     entry_points = {'console_scripts': ['sagaverage = sagittal_average.command:process']})
