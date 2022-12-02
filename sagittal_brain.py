7d40b41422fed1568a28e9f29a7a9a5df4862ecd
pkruzikova <p.kruziku@gmail.com>
2022-12-02 14:07:28 +0000

    added package-like structure

--- sagittal_brain.py
+++ sagittal_brain.py
@@ -3,7 +3,8 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import numpy as np
 
 
-def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv'):
+#def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv'):
+def run_averages(file_input, file_output):
     """
     Calculates the average through the coronal planes
     The input file should has as many columns as coronal planes
@@ -16,7 +17,7 @@ def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv')
 
     # Calculates the averages through the sagittal/horizontal planes
     # and makes it as a row vector
-    averages = planes.mean(axis=0)[np.newaxis, :]
+    averages = planes.mean(axis=1)[np.newaxis, :]
 
     # write it out on my file
     np.savetxt(file_output, averages, fmt='%.1f', delimiter=',')
