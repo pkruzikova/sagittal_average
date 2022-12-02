4e3e8f82a522ce8e31a9e685f569181ae29be8a5
pkruzikova <p.kruziku@gmail.com>
2022-12-02 14:07:02 +0000

    added package-like structure

---
+++ tests/test_sagittal_brain.py
@@ -0,0 +1,24 @@
+import numpy as np
+from ..sagittal_brain import run_averages
+#import subprocess
+from pathlib import Path
+
+TEST_DIR = Path(__file__).parent
+
+def test_average():
+    data_input = np.zeros((20, 20))
+    data_input[-1, :] = 1
+    np.savetxt(TEST_DIR / "brain_sample.csv", data_input, fmt='%d', delimiter=',')
+
+    #subprocess.run(["python", "sagittal_brain.py"])
+
+    expected = np.zeros(20)
+    expected[-1] = 1
+
+    #run python program
+    run_averages(file_input=TEST_DIR / "brain_sample.csv", file_output=TEST_DIR / "brain_average.csv")
+    output = np.loadtxt(TEST_DIR / "brain_average.csv", delimiter=',')
+
+    #output = np.loadtxt("brain_average.csv", delimiter=",")
+
+    assert np.array_equal(expected, output)
